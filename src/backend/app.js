319a67cead212f71501ba642167eb73c28c5349e
Ackeem <damanybailey@DamanyBileysMBP.localdomain>
2018-08-07 12:14:30 +0100

    23#

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
-
 const Animes = require('./animes');
+const path = require('path');
+const port = process.env.PORT || 3002;
 
 app.get('/rest/animes', (req, res) => res.send(Animes))
 
-app.listen(3002, () => console.log('Example app listening on port 3002!'))
+app.use( express.static(path.join(__dirname, '../../build')))
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
